1de03617cf25c8c194dee7f9462c176ac041b296
Anas Altaf <anasaltaf35@gmail.com>
2024-06-29 00:10:36 +0500

    Upadted

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,15 @@ void main() => runApp(const Xylophone());
 
 //SoundBox Widget
 class SoundBar extends StatelessWidget {
-  final String audiofilePath;
+  final int audiofileNumber;
   final Color barBackColor;
 
-  const SoundBar({required this.audiofilePath, required this.barBackColor});
+  const SoundBar({required this.audiofileNumber, required this.barBackColor});
 
   void audioPlayer() async {
     final AudioPlayer player = AudioPlayer();
     //await player.stop();
-    await player.play(AssetSource(audiofilePath));
+    await player.play(AssetSource('note$audiofileNumber.wav'));
   }
 
   @override
@@ -43,13 +43,13 @@ class Xylophone extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              SoundBar(audiofilePath: 'note1.wav', barBackColor: Colors.red),
-              SoundBar(audiofilePath: 'note2.wav', barBackColor: Colors.orange),
-              SoundBar(audiofilePath: 'note3.wav', barBackColor: Colors.yellow),
-              SoundBar(audiofilePath: 'note4.wav', barBackColor: Colors.green),
-              SoundBar(audiofilePath: 'note5.wav', barBackColor: Colors.teal),
-              SoundBar(audiofilePath: 'note6.wav', barBackColor: Colors.blue),
-              SoundBar(audiofilePath: 'note7.wav', barBackColor: Colors.purple),
+              SoundBar(audiofileNumber: 1, barBackColor: Colors.red),
+              SoundBar(audiofileNumber: 2, barBackColor: Colors.orange),
+              SoundBar(audiofileNumber: 3, barBackColor: Colors.yellow),
+              SoundBar(audiofileNumber: 4, barBackColor: Colors.green),
+              SoundBar(audiofileNumber: 5, barBackColor: Colors.teal),
+              SoundBar(audiofileNumber: 7, barBackColor: Colors.blue),
+              SoundBar(audiofileNumber: 8, barBackColor: Colors.purple),
             ],
           ),
         ),
